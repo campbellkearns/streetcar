538dcd4044adf54f1a9c221a08e8b1626797ee5a
Brandon Kearns <brandon.j.kearns@gmail.com>
2015-03-11 08:04:50 -0400

    Gruntfile proxy and .buildpacks

---
+++ client/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name streetCarApp
+ * @description
+ * # streetCarApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('streetCarApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
