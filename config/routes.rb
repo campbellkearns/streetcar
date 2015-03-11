00d39bc6ccb3d647cc8fe9f770c5dcf854c933a1
Brandon Kearns <brandon.j.kearns@gmail.com>
2015-03-11 08:05:29 -0400

    Gruntfile proxy and .buildpacks added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, except: [:new, :edit]
+  scope '/api' do
+    resources :users, except: [:new, :edit]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
