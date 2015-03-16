820b015873de1596d7ea8849e785c76b6cafad62
Brandon Kearns <brandon.j.kearns@gmail.com>
2015-03-16 14:23:24 -0400

    Adding map for new itinerary

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -3,6 +3,6 @@ class Itinerary < ActiveRecord::Base
   has_and_belongs_to_many :events
 
   validates :name, presence: true, length: { maximum: 250 }
-  #validates :date, presence: true
+  validates :date, presence: true
 
 end
